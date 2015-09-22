4fae82010fddc1ccddc07a1b1653a5e525bc6cd1
Pankaj Verma <pankajverma232@gmail.com>
2015-09-22 13:13:39 +0530

    Initial Commit

---
+++ AdServerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdServerDemo
+//
+//  Created by Pankaj Verma on 22/09/15.
+//  Copyright © 2015 Pankaj Verma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AdServerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AdServerDemo
+//
+//  Created by Pankaj Verma on 22/09/15.
+//  Copyright © 2015 Pankaj Verma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
